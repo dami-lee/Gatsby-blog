e83cf2f6fd4c56a46a2fa3ffc1170d7e657c09f6
dami-lee <dami223@daum.net>
2019-12-09 17:25:52 +0900

    Install gatsby-transformer-remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
